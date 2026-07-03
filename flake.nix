d71876a3ead3177fa018ee729d4b32edd07f4bfe
deymosxdeymos <galinnichola15@gmail.com>
2026-07-04 03:07:08 +0700

    flake: init

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "cfactoryai ThinkPad NixOS configuration";
+
+  nixConfig = {
+    experimental-features = [
+      "flakes"
+      "nix-command"
+      "pipe-operators"
+    ];
+
+    warn-dirty = false;
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    helium = {
+      url = "github:amaanq/helium-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    themes.url = "github:RGBCube/ThemeNix";
+
+    hjem = {
+      url = "github:feel-co/hjem";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hjem-rum = {
+      url = "github:snugnug/hjem-rum";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.hjem.follows = "hjem";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake
+      {
+        inherit inputs;
+      }
+      (
+        { lib, ... }:
+        let
+          inherit (lib.filesystem) listFilesRecursive;
+          inherit (lib.lists) filter;
+          inherit (lib.strings) hasSuffix;
+        in
+        {
+          systems = [
+            "x86_64-linux"
+          ];
+
+          imports = filter (hasSuffix ".mod.nix") (listFilesRecursive ./.);
+        }
+      );
+}
