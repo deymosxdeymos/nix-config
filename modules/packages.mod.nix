70c401ff2af2be331659bd6900d0479e2ceb9e44
deymosxdeymos <galinnichola15@gmail.com>
2026-07-04 03:11:58 +0700

    packages: init

---
+++ modules/packages.mod.nix
@@ -0,0 +1,47 @@
+{
+  flake.nixosModules.packages =
+    { pkgs, ... }:
+    {
+      nixpkgs.config.allowUnfree = true;
+
+      environment.systemPackages = [
+        pkgs.curl
+        pkgs.git
+        pkgs.nano
+        pkgs.vim
+        pkgs.wget
+      ];
+    };
+
+  flake.homeModules.packages-shell-utils =
+    { pkgs, ... }:
+    {
+      packages = [
+        pkgs.delta
+        pkgs.duf
+        pkgs.eza
+        pkgs.fd
+        pkgs.fzf
+        pkgs.hyperfine
+        pkgs.tokei
+        pkgs.tree
+      ];
+
+      rum.programs.direnv = {
+        enable = true;
+        integrations.nix-direnv.enable = true;
+        integrations.nushell.enable = true;
+      };
+
+      rum.programs.fzf.enable = true;
+
+      rum.programs.zoxide = {
+        enable = true;
+        flags = [
+          "--cmd"
+          "cd"
+        ];
+        integrations.nushell.enable = true;
+      };
+    };
+}
