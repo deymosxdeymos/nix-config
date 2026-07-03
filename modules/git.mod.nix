79c5e8ba85f0097154ae5bd356232b78882aaec0
deymosxdeymos <galinnichola15@gmail.com>
2026-07-04 03:11:57 +0700

    git: init

---
+++ modules/git.mod.nix
@@ -0,0 +1,30 @@
+{
+  flake.homeModules.git =
+    { lib, pkgs, ... }:
+    let
+      inherit (lib.generators) toGitINI;
+    in
+    {
+      packages = [
+        pkgs.gitMinimal
+        pkgs.lazygit
+      ];
+
+      xdg.config.files."git/config".generator = toGitINI;
+      xdg.config.files."git/config".value = {
+        user.name = "deymosxdeymos";
+        user.email = "galinnichola15@gmail.com";
+
+        init.defaultBranch = "main";
+        pull.rebase = true;
+        rebase.autoStash = true;
+        diff.tool = "difftastic";
+
+        fetch.fsckObjects = true;
+        receive.fsckObjects = true;
+        transfer.fsckobjects = true;
+
+        url."ssh://git@github.com/".insteadOf = "https://github.com/";
+      };
+    };
+}
