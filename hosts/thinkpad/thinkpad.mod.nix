2e4022cb7595943a3012d9812c258baac0bcfc17
deymosxdeymos <galinnichola15@gmail.com>
2026-07-04 03:11:58 +0700

    thinkpad: init

---
+++ hosts/thinkpad/thinkpad.mod.nix
@@ -0,0 +1,28 @@
+{
+  inputs,
+  lib,
+  self,
+  ...
+}:
+let
+  inherit (lib.attrsets) attrValues;
+in
+{
+  flake.nixosConfigurations.thinkpad = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+
+    specialArgs = {
+      inherit inputs self;
+    };
+
+    modules = attrValues self.nixosModules ++ [
+      ./hardware-configuration.nix
+
+      # Generic, model-agnostic ThinkPad tuning. If you know the exact model,
+      # swap these for a specific profile (e.g. lenovo-thinkpad-x1-...).
+      inputs.nixos-hardware.nixosModules.common-pc-laptop
+      inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
+      inputs.nixos-hardware.nixosModules.common-cpu-intel
+    ];
+  };
+}
